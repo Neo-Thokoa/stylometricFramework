bb1ce81741e47f112910c75eb264324b2b09f611
neo.thokoa <neo.thokoa@globetom.com>
2018-10-24 16:26:49 +0200

    HHP RIP

--- Sysytem Implementation/a_DataAquisition/retrieve_gmail.py
+++ Sysytem Implementation/a_DataAquisition/retrieve_gmail.py
@@ -6,24 +6,13 @@ from flask import Flask, request
 from flask_restful import Resource, Api
 from sqlalchemy import create_engine
 from json import dumps
-from flask.ext.jsonpify import jsonify
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
 
 def main():
-    """Shows basic usage of the Gmail API.
-    Lists the user's Gmail labels.
-    """
-    store = file.Storage('token.json')
-    creds = store.get()
-    if not creds or creds.invalid:
-        flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
-        creds = tools.run_flow(flow, store)
-    service = build('gmail', 'v1', http=creds.authorize(Http()))
-
     # Call the Gmail API
-    results = service.users().labels().list(userId='me').execute()
+    results = gmailauth()
     labels = results.get('labels', [])
 
     if not labels:
@@ -32,6 +21,21 @@ def main():
         print('Labels:')
         for label in labels:
             print(label['name'])
+
+def gmailauth():
+    """Shows basic usage of the Gmail API.
+        Lists the user's Gmail labels.
+        """
+    store = file.Storage('token.json')
+    creds = store.get()
+    if not creds or creds.invalid:
+        flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
+        creds = tools.run_flow(flow, store)
+    service = build('gmail', 'v1', http=creds.authorize(Http()))
+    print(service.users().labels())
+    # Call the Gmail API
+    return service.users().labels().list(userId='me').execute()
+
 
 if __name__ == '__main__':
     main()
